cd146c2d8515358fe380c2e007d04a05db445304
Sam Elliott <soe1@uakron.edu>
2024-03-01 09:43:46 -0500

    updated the SearchLocation component with the LocationInfo class, so we can get the latitude and longitude of the address the user inputs!

--- weather-app/src/SearchLocation.tsx
+++ weather-app/src/SearchLocation.tsx
@@ -4,9 +4,23 @@ import axios from 'axios'
 
 
 export default function SearchLocation() {
+
+    const [search, setSearch] = useState('');
+
+    class LocationInfo {
+
+        Lat: number;
+        Long: number;
+
+        constructor(lat: number, long: number) {
+            this.Lat = lat;
+            this.Long = long;
+        }
+    }
+
     return (
         <div>
-            <input className="outline outline-black" />
+            <input className="outline outline-black" value={search} />
             <button className="text-xl"> Search </button>
         </div>
     )

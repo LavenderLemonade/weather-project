c847d0dba074c24ede9ef37224d9a897e44aa9fb
Sam Elliott <soe1@uakron.edu>
2024-03-01 16:28:10 -0500

    we finally are getting the full array of objects!

--- weather-app/src/SearchLocation.tsx
+++ weather-app/src/SearchLocation.tsx
@@ -36,6 +36,26 @@ export default function SearchLocation() {
 
         })
     }
+    async function findWeatherConditions(url: string): Promise<Array<Object>> {
+        try {
+            const data = await axios.get(url);
+            return data.data.properties.periods;
+        }
+        catch (error) {
+            return [{}];
+        }
+    }
+
+    async function findWeatherConditionData(location: string): Promise<any> {
+        try {
+            const data = await axios.get(baseWeatherURL + encodeURIComponent(location).replace(/%20/g, ","));
+            return data.data;
+        }
+
+        catch (error) {
+            return '';
+        }
+    }
 
     async function findLongAndLat(search: string): Promise<Array<number>> {
         try {
@@ -51,7 +71,17 @@ export default function SearchLocation() {
     return (
         <div>
             <input className="outline outline-black" value={search} onChange={e => setSearch(e.target.value)} />
-            <button className="text-xl" onClick={() => { findLongAndLat(search).then((data) => { setCurrentLoc(data); }) }}> Search </button>
+            <button className="text-xl" onClick={() => {
+                findLongAndLat(search).then((data) => {
+                    return findWeatherConditionData(data.toString());
+                }).then((response) => {
+                    return findWeatherConditions(response.properties.forecast);
+                }
+
+
+                )
+                    .then((data) => { console.log(data) });
+            }}> Search </button>
 
             <h1> Current Lat: {currentLoc[0]} </h1>
             <h1> Current Long: {currentLoc[1]} </h1>

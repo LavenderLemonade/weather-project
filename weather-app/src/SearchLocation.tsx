768f4693fc8358b4b44196edc92499cde832e122
Sam Elliott <soe1@uakron.edu>
2024-03-01 13:50:17 -0500

    need to go back
    Revert "alright, we are getting the information about the current weather and we have all of the info about today in objects."
    
    This reverts commit 10f0b14405c2be66c26a5a21823719ce70df4e66.

--- weather-app/src/SearchLocation.tsx
+++ weather-app/src/SearchLocation.tsx
@@ -7,58 +7,19 @@ export default function SearchLocation() {
 
     const [search, setSearch] = useState('');
     const [currentLoc, setCurrentLoc] = useState<Array<number>>([])
-    const [currentForecastInfo, setCurrentForecastInfo] = useState('');
-
+    const [currentForecast, setCurrentForecast] = useState('');
 
     let baseLocURL = 'https://nominatim.openstreetmap.org/search?q=';
     let locFormatDetails = "&format=json&addressdetails=1";
     let baseWeatherURL = 'https://api.weather.gov/points/';
 
-    class forecastInfo {
-        Name: string;
-        Temp: number;
-        Description: string;
-        Speed: number;
-        DetailedForecast: string;
-
-        constructor(name: string, temp: number, desc: string, speed: number, details: string) {
-            this.Name = name;
-            this.Temp = temp;
-            this.Description = desc;
-            this.Speed = speed;
-            this.DetailedForecast = details;
-        }
-    }
-
-    class ForecastFactory {
-        createForecast(props: any) {
-            return new forecastInfo(props.name, props.temperature, props.shortForecast, props.windSpeed, props.detailedForecast);
-        }
-    }
-
-    const factory = new ForecastFactory();
-
-    const [forecastToday, setForecastToday] = useState<Array<forecastInfo>>([]);
-
-
     function getCurrentWeather(location: string) {
         console.log(encodeURIComponent(location));
         axios.get(baseWeatherURL + encodeURIComponent(location).replace(/%20/g, ",")).then((response) => {
-            setCurrentForecastInfo(response.data.properties.forecast);
+            setCurrentForecast(response.data.properties.forecast);
         }).then(
-            () => axios.get(currentForecastInfo).then((response) => {
+            () => axios.get(currentForecast).then((response) => {
                 console.log(response.data.properties.periods);
-                for (let i = 0; i < 2; i++) {
-                    setForecastToday(forecastToday => [...forecastToday, factory.createForecast({
-                        name: response.data.properties.periods[i].name,
-                        temperature: response.data.properties.periods[i].temperature,
-                        shortForecast: response.data.properties.periods[i].shortForecast,
-                        windSpeed: response.data.properties.periods[i].windSpeed,
-                        detailedForecast: response.data.properties.periods[i].detailedForecast
-                    })]);
-                }
-
-                console.log(forecastToday);
             })
         )
 

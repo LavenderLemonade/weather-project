cb8c0b117cf19add2fd57becb4493ae7afcf0a65
Sam Elliott <soe1@uakron.edu>
2024-03-01 12:39:20 -0500

    been doing a lot so want to commit: we are using the National Weather Service API: feeing the lat and long we got from the Nominatim API into it to get the forecasts back, and I am currently testing how to get the object with the info back. Probably a factory function in my future.

--- weather-app/src/SearchLocation.tsx
+++ weather-app/src/SearchLocation.tsx
@@ -7,16 +7,32 @@ export default function SearchLocation() {
 
     const [search, setSearch] = useState('');
     const [currentLoc, setCurrentLoc] = useState<Array<number>>([])
+    const [currentForecast, setCurrentForecast] = useState('');
 
     let baseLocURL = 'https://nominatim.openstreetmap.org/search?q=';
     let locFormatDetails = "&format=json&addressdetails=1";
+    let baseWeatherURL = 'https://api.weather.gov/points/';
 
+    function getCurrentWeather(location: string) {
+        console.log(encodeURIComponent(location));
+        axios.get(baseWeatherURL + encodeURIComponent(location).replace(/%20/g, ",")).then((response) => {
+            setCurrentForecast(response.data.properties.forecast);
+        }).then(
+            () => axios.get(currentForecast).then((response) => {
+                console.log(response.data.properties.periods);
+            })
+        )
+
+
+    }
 
     function getLocationData(searchQuery: string) {
         axios.get(baseLocURL + encodeURIComponent(searchQuery).replace(/%20/g, "+") + locFormatDetails).then((response) => {
             let newLoc: number[] = [response.data[0].lat, response.data[0].lon];
             setCurrentLoc(newLoc);
         })
+
+        getCurrentWeather(currentLoc.toString());
     }
 
     return (
